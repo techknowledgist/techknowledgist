bb33c82382e14e07809d35116a868e3e66c33664
Marc Verhagen <marc@cs.brandeis.edu>
2012-09-23 03:37:48 -0400

    Some initial scaffolding; added file lists.

--- runtime/main.py
+++ runtime/main.py
@@ -3,13 +3,71 @@ import os, sys
 script_path = os.path.abspath(sys.argv[0])
 script_dir = os.path.dirname(script_path)
 code_dir = os.path.split(script_dir)[0]
-
-print 1, script_path
-print 2, script_dir
-print 3, code_dir
-
 sys.path.append(code_dir)
 
-print sys.path
-import utils.docstructure
+import getopt
+from utils.docstructure.main import Parser
+#from utils.tries
+
+
+class TechnologyTagger(object):
+
+    """
+    Main class, reponsible for:
+    - reading the list of files to process
+    - running the Document Structure Parser
+    - reading in the list of technologies (into a trie)
+    - reading patterns (future functionailty)
+    - looking up technologies in parts of the documents
+    - using weights depending on location in document (future functionailty)
+    - collecting and storing results
+    - calculate maturity scores for each document
+    - calling the Aggregator to calculate year-by-year scores and RDG score
+    - calling the Exporter to generate output
+    """
+
+    def __init__(self, language, file_list):
+        self.language = language
+        self.file_list = file_list
+        self.files = [l.strip() for l in open(file_list).readlines()]
+        
+    def __str__(self):
+        return "<TechnologyTagger on \"%s\" language=\"%s\">" % (self.file_list, self.language)
+
+    def process_files(self):
+        for filename in self.files:
+            self.process_file(filename)
+
+    def process_file(self, filename):
+        """The workhorse method."""
+        
+    
+class Aggregator(object): pass
+
+class Exporter(object): pass
+
+
+
+
+if __name__ == '__main__':
+
+    try:
+        (opts, args) = getopt.getopt(sys.argv[1:], 'l:')
+    except getopt.GetoptError, err:
+        print str(err)
+        usage()
+        sys.exit(2)
+
+    language = 'ENGLISH'
+    for opt, val in opts:
+        if opt == '-l': language = val
+
+    file_list = args[0]
+
+    ttagger = TechnologyTagger(language, file_list)
+
+    print ttagger
+    
+    
+
 
---
+++ utils/00-readme.txt
@@ -0,0 +1,24 @@
+Note that some of these utilities are actually exported from other repositories and that
+they are not maintained here. These also do not need to be checked into the git
+repository. This is the case, for example, for docstructure and tries.
+
+To export the document structure parser, do something like this:
+
+% cd ~marc/Dropbox/FUSE/document-processing/structure/git
+% ./utils/export.sh /home/j/corpuswork/fuse/code/utils
+% cd /home/j/corpuswork/fuse/code/utils
+% mv docstructure docstructure-saved
+% mv docstructure-20120922 docstructure
+
+The tries utilities is taken from my utilities repository. To export from that repository,
+do something like th efollowing:
+
+% cd ~marc/Dropbox/code/misc
+...
+
+TODO: write a script to import utilities from here
+
+
+Marc Verhagen
+marc@cs.brandeis.edu
+September 2012
