e2ec46837ff274ce41cd6356388e2a72d10492b7
Marc Verhagen <marc@cs.brandeis.edu>
2013-12-05 23:59:48 -0500

    BAE-requested changes for runtime keyterms code.

--- ontology/runtime/keyterms.py
+++ ontology/runtime/keyterms.py
@@ -1,27 +1,65 @@
-"""
+"""Run the keyterm extraction code on a list of files.
 
-Run the keyterm extraction code on a list of files.
 
 Usage:
 
-    $ python keyterms.py FILE
-    $ python keyterms.py --verbose FILE
+   $ python keyterms.py OPTIONS
 
-    FILE has a list of paths to the input files. In verbose mode, messages will
-    be printed to the terminal and temporary files in workspace/tmp will not be
-    removed.
+OPTIONS:
 
-Needed input files are text files and the BAE fact files. The script adds the
-extensions to the path, for example, a path data/US6682507B2 would be expanded
-to include both data/US6682507B2.txt and data/US6682507B2.fact. There is an
-example file in workspace/data/list-010.txt. The script relies on two kinds of
-facts in the fact file, both from the generic structural parsing by BAE:
+--filelist FILE
+   Contains a list of paths to the input files. This is the only required
+   option.
+
+--run-id STRING
+   The optional run-id option defines an identifier for the current run. The
+   default is to use the current timestamp. The run-id determines where
+   temporary and results file are written to. For example, with a run-id of
+   'run-018', temporary files are written to workspace/tmp/run-018 and result
+   files are written to workspace/results/run-018.
+
+--verbose
+   In verbose mode, messages will be printed to the terminal and temporary files
+   in workspace/tmp will not be removed. 
+
+--mallet-dir PATH
+--stanford-tagger-dir PATH
+   These can be used to overrule the default directories for the mallet
+   classifier and the stanford tagger. There are actually two ways of doing
+   this. One is to edit the config.py file in this directory. The other is to
+   use these options. If both are used, the command line options overrule the
+   values in config.py. See config.py for more information.
+
+--condense-results
+   When this flag is on, not all result files will be present in the results
+   directory. Only the two tab files (iclassify.MaxEnt.label.merged.tab and
+   iclassify.MaxEnt.label.relations.tab) and the info file will be written.
+
+
+Typical examples:
+
+   $ python keyterms.py --filelist workspace/data/list-010.txt
+
+
+Needed input files are text files and the BAE fact files. Each line in FILE is
+in one of two formats. In the first format a line has a path to the text file,
+followed by a tab, followed by the path to the fact file. In the second format,
+one file is listed without the extension. In that case, the script assumes that
+text and fact files are in the same directory and adds the extensions to the
+path, for example, a path data/US6682507B2 would be expanded to include both
+data/US6682507B2.txt and data/US6682507B2.fact. There is an example file in
+workspace/data/list-010.txt with examples of both formats (which may both be
+used in the same file).
+
+The script relies on two kinds of facts in the fact file, both from the generic
+structural parsing by BAE:
 
     STRUCTURE TYPE="TITLE" START=3302 END=3329
     STRUCTURE TYPE="ABSTRACT" START=10475 END=11178
 
-Results are printed to a set of files in this directory, they all have the
-prefix 'iclassify':
+Results are printed to a set of files in the results directory (see the
+explanation of --run-id above for where exactly in the results directory), they
+all have the prefix 'iclassify':
 
     iclassify.info - some minimal info about the completed run
     iclassify.mallet - input file for the mallet classifier
@@ -116,7 +154,7 @@ Runtime performance on the set of 200 sample files provided by BAE:
 """
 
 
-import os, sys, codecs, subprocess, time
+import os, sys, codecs, subprocess, time, getopt
 
 script_path = os.path.abspath(sys.argv[0])
 script_dir = os.path.dirname(script_path)
@@ -138,57 +176,83 @@ from ontology.runtime.utils.text import parse_fact_line
 MODEL = '../classifier/data/models/inventions-standard-20130713/'
 
 
-def process(filelist):
-    update_directories()
+def process(filelist, run_id, mallet_path, stanford_path, condense_results):
+    update_directories(mallet_path, stanford_path)
+    os.mkdir('workspace/tmp/' + run_id)
+    os.mkdir('workspace/results/' + run_id)
     t1 = time.time()
-    infiles = [ f.strip() for f in open(filelist).readlines()]#[:10]
+    infiles = read_filelist(filelist)
     chk_files = []
     tagger = txt2tag.get_tagger('en')
-    if VERBOSE:
-        print '[process] pre-processing files'
-    for infile in infiles:
-        if VERBOSE:
-            print '  ', infile
-        basename = os.path.basename(infile)
-        txt_file = os.path.join('workspace/tmp', basename + '.txt')
-        tag_file = os.path.join('workspace/tmp', basename + '.tag')
-        chk_file = os.path.join('workspace/tmp', basename + '.chk')
+    if VERBOSE: print '[process] pre-processing files'
+    for text_file, fact_file in infiles:
+        if VERBOSE: print '  ', text_file
+        basename = os.path.splitext(os.path.basename(text_file))[0]
+        txt_file = os.path.join("workspace/tmp/%s/%s.txt" % (run_id, basename))
+        tag_file = os.path.join("workspace/tmp/%s/%s.tag" % (run_id, basename))
+        chk_file = os.path.join("workspace/tmp/%s/%s.chk" % (run_id, basename))
         chk_files.append(chk_file)
-        run_xml2txt(infile, txt_file)
+        run_xml2txt(text_file, fact_file, txt_file)
         run_txt2tag(txt_file, tag_file, tagger)
         run_tag2chk(tag_file, chk_file)
-    run_classifier(chk_files)
-    cleanup()
-    if VERBOSE:
-        print "Time elapsed: %f" % (time.time() - t1)
-        
-def update_directories():
+    run_classifier(chk_files, run_id, condense_results)
+    cleanup(run_id)
+    if VERBOSE: print "Time elapsed: %f" % (time.time() - t1)
+
+def default_id():
+    return time.strftime('%Y%m%d-%H%M%S')
+
+def update_directories(mallet_path, stanford_path):
     """Updates the MALLET_DIR and STANFORD_TAGGER_DIR paths in the module
-    ontology.creation.config with the values in the local module named
-    ontology.runtime.config."""
+    ontology.creation.config and ontology.classifier.config with the values in
+    the local module ontology.runtime.config or the values handed in by command
+    line options."""
     # TODO: this a bit of a hack and I would like to have a better way to deal
     # with all the directory settings.
     import config
     import ontology.creation.config
     import ontology.classifier.config
+    # check the local config
     for var in ('MALLET_DIR', 'STANFORD_TAGGER_DIR'):
-        try:
-            dir = config.__dict__[var]
-            if os.path.isdir(dir):
-                ontology.creation.config.__dict__[var] = dir
-                ontology.classifier.config.__dict__[var] = dir
+        path = config.__dict__.get(var)
+        if path is not None:
+            if os.path.isdir(path):
+                ontology.creation.config.__dict__[var] = path
+                ontology.classifier.config.__dict__[var] = path
             else:
-                print "WARNING: not a directory: '%s'" % dir
-                print "         ontology.creation.config.%s not updated" % var
-                print "         ontology.classifier.config.%s not updated" % var
-        except KeyError:
-            pass
-    #print 'MALLET_DIR:', ontology.classifier.config.MALLET_DIR
-    #exit()
+                exit("Invalid %s directory: %s" % (var, path))
+    # check the values from the command line options
+    if mallet_path is not None:
+        if os.path.isdir(mallet_path):
+            ontology.creation.config.__dict__['MALLET_DIR'] = mallet_path
+            ontology.classifier.config.__dict__['MALLET_DIR'] = mallet_path
+        else:
+            exit("Invalid mallet directory: " + mallet_path)
+    if stanford_path is not None:
+        if os.path.isdir(stanford_path):
+            ontology.creation.config.__dict__['STANFORD_TAGGER_DIR'] = stanford_path
+            ontology.classifier.config.__dict__['STANFORD_TAGGER_DIR'] = stanford_path
+        else:
+            exit("Invalid stanford directory: " + stanford_path)
+    if VERBOSE:
+        print 'MALLET_DIR =', ontology.classifier.config.MALLET_DIR
+        print 'STANFORD_TAGGER_DIR =', ontology.creation.config.STANFORD_TAGGER_DIR
 
-def run_xml2txt(infile, outfile):
-    text_file = infile + '.txt'
-    fact_file = infile + '.fact'
+def read_filelist(filelist):
+    infiles = []
+    for line in open(filelist):
+        files = line.strip().split("\t")
+        if len(files) == 1:
+            infiles.append([files[0]+'.txt', files[0]+'.fact'])
+        elif len(files) == 2:
+            infiles.append([files[0], files[1]])
+        else:
+            print "WARNING: unexpected line in filelist"
+            print "        ", line
+    return infiles
+
+
+def run_xml2txt(text_file, fact_file, outfile):
     fh_in = codecs.open(text_file, encoding='utf-8')
     fh_out = codecs.open(outfile, 'w', encoding='utf-8')
     text = fh_in.read()
@@ -219,33 +283,33 @@ def run_tag2chk(tag_file, chk_file):
     tag2chunk.Doc(tag_file, chk_file, '9999', 'en',
                   filter_p=False, chunker_rules='en', compress=False)
 
-def cleanup():
+def cleanup(run_id):
     """Remove all temporary files unless in verbose mode."""
-    tmp_dir = "workspace/tmp"
-    filelist = [f for f in os.listdir(tmp_dir) if not f == '.gitignore']
+    tmp_dir = "workspace/tmp/" + run_id
     if not VERBOSE:
-        for f in filelist:
+        for f in os.listdir(tmp_dir):
             os.remove(os.path.join(tmp_dir, f))
 
-def run_classifier(chk_files):
-    mallet_file = os.path.join('workspace', 'tmp', 'iclassify.mallet')
+def run_classifier(chk_files, run_id, condense_results):
+    results_dir = os.path.join('workspace', 'results', run_id)
+    mallet_file = os.path.join(results_dir, 'iclassify.mallet')
     with codecs.open(mallet_file, "w", encoding='utf-8') as s_mallet:
         for chk_file in chk_files:
             add_phr_feats_file(chk_file, s_mallet)
-    patent_invention_classify(MODEL, 'workspace/tmp', verbose=VERBOSE)
-    corpus = 'workspace/tmp'
+    patent_invention_classify(MODEL, results_dir, verbose=VERBOSE)
     label_file='iclassify.MaxEnt.label'
-    classification = 'workspace/tmp'
-    create_info_file(classification)
+    create_info_file(results_dir)
     command = "cat %s/%s | egrep -v '^name' | egrep '\|.*\|' | python %s > %s/%s" \
-              % (classification, 'iclassify.MaxEnt.out', '../classifier/invention_top_scores.py',
-                 classification, label_file)
+              % (results_dir, 'iclassify.MaxEnt.out', '../classifier/invention_top_scores.py',
+                 results_dir, label_file)
     if VERBOSE: print '$', command
     subprocess.call(command, shell=True)
-    process_label_file(corpus, classification, label_file, VERBOSE)
-    # move the results from the temporary workspace directory
-    command = "mv %s/iclassify* ." % classification
-    subprocess.call(command, shell=True)
+    process_label_file(results_dir, results_dir, label_file, VERBOSE)
+    if condense_results:
+        for fname in ['iclassify.MaxEnt.label', 'iclassify.MaxEnt.label.cat',
+                      'iclassify.MaxEnt.label.merged', 'iclassify.MaxEnt.out',
+                      'iclassify.MaxEnt.stderr', 'iclassify.mallet' ]:
+            os.remove(os.path.join(results_dir, fname))
 
 def create_info_file(classification):
     with open(os.path.join(classification, 'iclassify.info'), 'w') as fh:
@@ -257,9 +321,23 @@ def create_info_file(classification):
 
 if __name__ == '__main__':
 
+    options = ['runid=', 'mallet-dir=', 'stanford-tagger-dir=',
+               'filelist=', 'verbose', 'condense-results']
+    (opts, args) = getopt.getopt(sys.argv[1:], '', options)
+
     VERBOSE = False
-    filelist = sys.argv[1]
-    if len(sys.argv) > 2 and sys.argv[1] == '--verbose':
-        VERBOSE = True
-        filelist = sys.argv[2]
-    filelist = process(filelist)
+    filelist = None
+    run_id = default_id()
+    mallet_path = None
+    stanford_path = None
+    condense_results = False
+
+    for opt, val in opts:
+        if opt == '--verbose': VERBOSE = True
+        if opt == '--filelist': filelist = val
+        if opt == '--run-id':  run_id = val
+        if opt == '--mallet-dir': mallet_path = val
+        if opt == '--stanford-tagger-dir': stanford_path = val
+        if opt == '--condense-results': condense_results = True
+
+    process(filelist, run_id, mallet_path, stanford_path, condense_results)
