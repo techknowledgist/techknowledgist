48724240b0d445334f0222a4c386ee3de70b8248
Marc Verhagen <marc@cs.brandeis.edu>
2013-12-27 12:41:26 -0500

    Made script to extract publication and application date more general.

--- utils/create_date_idxs.py
+++ utils/create_date_idxs.py
@@ -1,7 +1,17 @@
 """
 
-A standalone utility to take a bunch of files from a list, XML-parse the files
-and extract some dates.
+A standalone utility to take a bunch of files from a list, parse the files using
+an xml parser or string matching and extract the application date and the
+publication date.
+
+Usage:
+
+    $ python create_date_idx.py INFILE OUTFILE COUNT SKIP 
+
+    Parses INFILE and creates OUTFILE which has three fields for each file, an
+    application date, a publicaiton date and the filepath. Writes warning to
+    date_warnings.txt. If COUNT is given, limits the number of files processed,
+    if SKIP is also given, then the first SKIP files are skipped.
 
 Expects each line of the input to start like this:
 
@@ -9,6 +19,13 @@ Expects each line of the input to start like this:
 
 That is, a year followed by a path. There can be more information after the long
 path (for example a short path as often used in the corpus config files).
+
+Alternatively, it allows this kind of input:
+
+    4577022A    /home/j/corpuswork/fuse/FUSEData/2013-04/ln_uspto/1986/004/US4577022A.xml
+
+This is the format as used in ln_uspto.all.shuffled.txt (and the ordered
+version) in the lists in /home/j/corpuswork/fuse/FUSEData/lists/.
 
 Perhaps belongs in ontology/utils (and perhaps patent_index.py belongs there as
 well).
@@ -21,10 +38,10 @@ from xml.dom.minidom import parse
 
 SKIP = 1000
 COUNT = 999999999999999999999
-if len(sys.argv) > 2: COUNT = int(sys.argv[2])
-if len(sys.argv) > 3: SKIP = int(sys.argv[3])
+if len(sys.argv) > 3: COUNT = int(sys.argv[3])
+if len(sys.argv) > 4: SKIP = int(sys.argv[4])
 fh = open(sys.argv[1])
-fh_results = open('date_idx.txt', 'w')
+fh_results = open(sys.argv[2], 'w')
 fh_warnings = open('date_warnings.txt', 'w')
 sys.stderr.write("Processing %d files from %s\n" % (COUNT, fh.name))
 
