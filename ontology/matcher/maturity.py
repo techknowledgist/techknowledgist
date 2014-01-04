631d437ae7edd9ab573feda815c9794bca805778
Marc Verhagen <marc@cs.brandeis.edu>
2014-01-03 23:38:06 -0500

    Updates to code that generates maturity time series.

--- ontology/matcher/maturity.py
+++ ontology/matcher/maturity.py
@@ -6,6 +6,26 @@ Runs on the results of the classifier and the patterns matcher. Creates a series
 of tab-separated files that were created in September 2013 as input to the BAE
 system. These files could also be input to the ontology.
 
+Usage:
+
+    $ python maturity.py
+
+    No command line arguments, but some global variables may need to be edited.
+
+Note that for this to work, you need three kinds of input:
+
+    1- files with classification results for each subcorpus, needed is the
+       classify.MaxEnt.out.s3.scores.sum file which was created with
+       ../classifier/run_iclassifier.py.
+
+    2- results from the matcher in match.results.summ.txt
+
+    3- the list of bad terms, created with ../classifier/utils/filter.py,
+       expected to be in the subdirectories of the classifications directory
+
+    4- the list with terms with frequency > 25, created with merge.py and
+       split_terms_on_frequency.py, both in ../classifier/utils, expected to be
+       in the classifications directory
 
 For each year, there are two tab-separated file with term to maturity score
 mappings. The files are named maturity-freq-based-YEAR.txt and
@@ -54,17 +74,26 @@ YEARS = [1997, 1998, 1999, 2000, 2001, 2002, 2003, 2004, 2005, 2006, 2007]
 
 # Directories and files
 
-CLASS_DIR = '/home/j/corpuswork/fuse/FUSEData/corpora/cs-500k/classifications'
-CLASS_SUFFIX = '-technologies-standard-1000'
-CLASS_FILE = 'classify.MaxEnt.out.s3.scores.sum'
+CORPUS_DIR = '/home/j/corpuswork/fuse/FUSEData/corpora/ln-cs-500k'
+CORPUS_DIR = '/home/j/corpuswork/fuse/FUSEData/corpora/ln-all-600k'
 
-MATCH_DIR = '/home/j/corpuswork/fuse/FUSEData/corpora/cs-500k/subcorpora'
-MATCH_SUFFIX = '/data/o2_matcher/batch-01/'
-MATCH_FILE = 'match.results.summ.txt'
+if CORPUS_DIR.endswith('ln-cs-500k'):
+    CLASS_DIR = os.path.join(CORPUS_DIR, 'classifications')
+    CLASS_SUFFIX = '-technologies-standard-1000'
+    CLASS_FILE = 'classify.MaxEnt.out.s3.scores.sum'
+    MATCH_DIR = os.path.join(CORPUS_DIR, 'subcorpora')
+    MATCH_SUBDIR = 'data/o2_matcher/batch-01'
+    MATCH_FILE = 'match.results.summ.txt'
+elif CORPUS_DIR.endswith('ln-all-600k'):
+    CLASS_DIR = os.path.join(CORPUS_DIR, 'classifications')
+    CLASS_PREFIX = 'technologies-ds1000-all-'
+    CLASS_FILE = 'classify.MaxEnt.out.s3.scores.sum'
+    MATCH_DIR = os.path.join(CORPUS_DIR, 'subcorpora')
+    MATCH_SUBDIR = 'data/o2_matcher/maturity'
+    MATCH_FILE = 'match.results.summ.txt'
 
 TERMS_FILE = CLASS_DIR + os.sep + 'all_terms.0025.txt'
 
-CORPUS_DIR = '/home/j/corpuswork/fuse/FUSEData/corpora/cs-500k'
 TIME_SERIES_DIR = CORPUS_DIR + '/time-series/maturity-scores'
 
 
@@ -72,16 +101,29 @@ TIME_SERIES_DIR = CORPUS_DIR + '/time-series/maturity-scores'
 
 # Adjusting for the size of a corpus for a year, YEAR_SIZES is derived by taking
 # a "wc -l" on the files.txt in the corpus
-YEAR_SIZES = {
+YEAR_SIZES_CS_500K = {
     1997: 18555, 1998: 19875, 1999: 22254, 2000: 28576, 2001: 57429, 2002: 51058,
     2003: 49212, 2004: 48869, 2005: 46493, 2006: 33290, 2007: 9058 }
-LARGEST_SIZE = 57429
+YEAR_SIZES_ALL_600K = {
+    1997: 15941, 1998: 15878, 1999: 17412, 2000: 19533, 2001: 36832, 2002: 38482,
+    2003: 42439, 2004: 43903, 2005: 44493, 2006: 43412, 2007: 40715 }
+
+corpus = "ln-cs-500k"
+corpus = "ln-all-600k"
+
+if corpus == "ln-cs-500k":
+    YEAR_SIZES = YEAR_SIZES_CS_500K
+elif corpus == "ln-all-600k":
+    YEAR_SIZES = YEAR_SIZES_ALL_600K
+
+LARGEST_SIZE = max(YEAR_SIZES.values())
 
 
 # Adjusting the match score with the upper bound. The first variable needs to be
 # set manually by finding term-year pair with the highest number of matches. For
 # convenience, this number is printed each time this script runs.
-MAX_NUMBER_OF_MATCHES = 90439
+MAX_NUMBER_OF_MATCHES = 90439  # ln-cs-500k
+MAX_NUMBER_OF_MATCHES = 26542  # ln-all-600k
 MATCHES_ADJUSTMENT = math.log(MAX_NUMBER_OF_MATCHES)
 
 
@@ -161,22 +203,29 @@ def calculate_maturity_scores(maturity_data):
 
         
 if __name__ == '__main__':
-    
+
     terms = read_terms()
     maturity_data = {}
     
     for year in YEARS:
         print
         print year
-        tech_file = "%s/%s%s/%s" % (CLASS_DIR, year, CLASS_SUFFIX, CLASS_FILE)
-        match_file = "%s/%s%s/%s" % (MATCH_DIR, year, MATCH_SUFFIX, MATCH_FILE)
+        if CORPUS_DIR.endswith('ln-cs-500k'):
+            tech_file = "%s/%s%s/%s" % (CLASS_DIR, year, CLASS_SUFFIX, CLASS_FILE)
+        elif CORPUS_DIR.endswith('ln-all-600k'):
+            tech_file = "%s/%s%s/%s" % (CLASS_DIR, CLASS_PREFIX, year, CLASS_FILE)
+        else:
+            exit(CORPUS_DIR)
+        match_file = "%s/%s/%s/%s" % (MATCH_DIR, year, MATCH_SUBDIR, MATCH_FILE)
+        print '  ', tech_file
+        print '  ', match_file
         term_matches = read_matches(match_file)
         c = 0
         print "   Reading tech file..."
         for line in codecs.open(tech_file):
             c += 1
-            if c % 1000000 == 0: print '     ', c
-            #if c > 1000000: break
+            if c % 100000 == 0: print '     ', c
+            #if c > 200000: break
             term, score, doc_count, min, max = line.split("\t")
             score = float(score)
             doc_count = int(doc_count)
