bb20eabb3c7a86a3ceb67437780c9a7b22d05db2
Marc Verhagen <marc@cs.brandeis.edu>
2015-02-16 12:52:06 -0500

    Minor fixes.

--- ontology/time_series/collect_keyterms.py
+++ ontology/time_series/collect_keyterms.py
@@ -1,8 +1,19 @@
+"""
+
+Collect keyterms from classification files and write them out as time series.
+
+Usage:
+
+    $ python collect_keyterms.py
+
+To control parameters for the script, edit the values of YEARS, CORPUS,
+KEYTERMS_CLASS and KEYTERMS_SERIES.
+
+"""
+
 
 import os, sys, codecs, StringIO
-
 from utils import filter_term_en, filter_term
-
 sys.path.append(os.path.abspath('../..'))
 from ontology.utils.file import open_input_file
 
@@ -15,6 +26,7 @@ YEAR_STRINGS = ["%s" % y for y in YEARS]
 CORPUS = '/home/j/corpuswork/fuse/FUSEData/corpora/ln-us-all-600k'
 KEYTERMS_CLASS =  os.path.join(CORPUS, 'classifications', 'keyterms-20150215')
 KEYTERMS_SERIES = os.path.join(CORPUS, 'time-series-v4', 'keyterms')
+#KEYTERMS_SERIES = '.'
 
 
 def read_roles(year):
@@ -30,9 +42,8 @@ def read_roles(year):
         roles[term][role] = roles.setdefault(term,{}).get(role, 0) + 1
     return roles
 
-# TODO: this is not writing to the right location!!!
 def print_roles(roles, year):
-    print "Writing to", os.path.join(KEYTERMS_SERIES, 'keyterms', "roles-*-%d.txt" * year)
+    print "Writing roles to", KEYTERMS_SERIES
     # use StringIO because writing to nsf-share line-by-line is sooooo slow
     s_ct = StringIO.StringIO()
     s_ca = StringIO.StringIO()
@@ -46,12 +57,14 @@ def print_roles(roles, year):
         s_ct.write("%s\t%d\t%.2f\n" % (term, ct, float(ct)/(all+0.00000001)))
         s_ca.write("%s\t%d\t%.2f\n" % (term, ca, float(ca)/(all+0.00000001)))
         s_i.write("%s\t%d\t%.2f\n" % (term, i, float(i)/(all+0.00000001)))
-    fh_ct = codecs.open(os.path.join(KEYTERMS_SERIES, "roles-ct-%s.txt" % year), 'w', encoding='utf8')
-    fh_ca = codecs.open(os.path.join(KEYTERMS_SERIES, "roles-ca-%s.txt" % year), 'w', encoding='utf8')
-    fh_i = codecs.open(os.path.join(KEYTERMS_SERIES, "roles-i-%s.txt" % year), 'w', encoding='utf8')
-    fh_ct.write(s_ct.getvalue())
-    fh_ca.write(s_ca.getvalue())
-    fh_i.write(s_i.getvalue())
+    print_from_stream(s_ct, 'ct', year)
+    print_from_stream(s_ca, 'ca', year)
+    print_from_stream(s_i, 'i', year)
+
+def print_from_stream(stream, role, year):
+    outfile = os.path.join(KEYTERMS_SERIES, "roles-%s-%s.txt" % (role, year))
+    fh = codecs.open(outfile, 'w', encoding='utf8')
+    fh.write(stream.getvalue())
 
 
 if __name__ == '__main__':
